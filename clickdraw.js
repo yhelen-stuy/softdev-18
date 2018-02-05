6fd3b4a1abd22a954f0c7c30169fbdf04e1d5963
Helen Ye <00yhelen@gmail.com>
2018-02-05 16:04:49 -0500

    Change file names

--- clickdraw.js
+++ clickdraw.js
@@ -1,61 +1,61 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
+var clearB = document.getElementById("clear");
+var toggleB = document.getElementById("toggle");
+
 var h = c.getAttribute("height");
 var w = c.getAttribute("width");
-var sq_side = h / 25
+var r = h / 50;
 
 var pi = Math.PI;
 
-var toggleB = document.getElementById("toggle");
-var clearB = document.getElementById("clear");
-
-// Whether to draw a dot
-var drawDot = true;
-
-var changeDrawing = function(e) {
-    e.preventDefault();
-    drawDot = !drawDot;
-}
+var cX = -1;
+var cY = -1;
 
 var drawCircle = function(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, sq_side / 2, 0, 2 * pi);
-    ctx.fillStyle = "#000000";
+    ctx.arc(x, y, r, 0, 2 * pi);
+    ctx.strokeStyle = "#000000";
     ctx.stroke();
     ctx.fillStyle = "#ff0000";
     ctx.fill();
+    ctx.moveTo(x, y);
 }
 
-var drawSquare = function(x, y) {
-    x -= (sq_side / 2)
-    y -= (sq_side / 2)
-    ctx.fillStyle = "#00ff00";
-    ctx.fillRect(x, y, sq_side, sq_side);
-    // Draw outine
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(x, y, sq_side, 1);
-    ctx.fillRect(x, y, 1, sq_side);
-    ctx.fillRect(x + sq_side - 1, y, 1, sq_side);
-    ctx.fillRect(x, y + sq_side - 1, sq_side, 1);
+var connect = function(x0, y0, x1, y1) {
+    ctx.beginPath();
+    ctx.strokeStyle = "#0000ff";
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
 }
 
 var draw = function(e) {
     e.preventDefault();
     var x = e.offsetX;
     var y = e.offsetY;
-    if (drawDot) {
-        drawCircle(x, y);
+    drawCircle(x, y);
+    if (cX < 0 || cY < 0) {
+        cX = e.offsetX;
+        cY = e.offsetY;
     } else {
-        drawSquare(x, y);
+        connect(cX, cY, x, y);
+        cX = x;
+        cY = y;
     }
 }
 
-var clearCanvas = function (e) {
+var reset = function() {
+    cX = -1;
+    cY = -1;
+}
+
+var clearCanvas = function(e) {
     e.preventDefault();
     ctx.clearRect(0, 0, h, w);
+    reset();
 }
 
 clearB.addEventListener('click', clearCanvas);
-toggleB.addEventListener('click', changeDrawing);
 
 c.addEventListener('click', draw);
